ddd936e21058c2f78bf2bf928f7433d3d410c79b
Dhanush Pillai <157956058+Dh4nush11@users.noreply.github.com>
2026-07-30 18:56:42 +0530

    Create ReportService.java

---
+++ ReportService.java
@@ -0,0 +1,14 @@
+package com.dhanush.scheduler.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportService {
+
+    public String generate(){
+
+        return "Task Report Generated Successfully";
+
+    }
+
+}
